1a2a1def4a343d0c81a69feb241c488bd6ce28b7
Chalermrat Srinara <schalermrat@gmail.com>
2017-07-12 14:52:10 +0700

    feat(server): Init server

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(3333, () => {
+  console.log('listening on port 3333')
+})
